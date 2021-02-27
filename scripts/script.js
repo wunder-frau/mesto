eee18755382ba2bc777981bf95c65c68eafb3991
Iryna Krukovich <wunder.frau@yandex.by>
2021-02-28 01:50:48 +0300

    Make elements:form__input form__input_bio(_name) and java input

--- scripts/script.js
+++ scripts/script.js
@@ -4,21 +4,24 @@ let hidePopupButton = document.querySelector('#hide-popup');
 let formElement = document.querySelector('.form');
 let submitButton = document.querySelector('.form__button_submit');
 
+
 function popupToggle() {
+  document.querySelector('.form__input_name').value = document.querySelector('.profile__title').textContent;
+  document.querySelector('.form__input_bio').value = document.querySelector('.profile__subtitle').textContent;
   popup.classList.toggle('popup_opend');
 }
 
 function formSubmitHandler (event) {
   event.preventDefault();
-  let inputName = document.querySelector('.form__input-name').value;
-  let jobInput = document.querySelector('.form__input-bio').value;
+  let inputName = document.querySelector('.form__input_name').value;
+  let jobInput = document.querySelector('.form__input_bio').value;
 
   document.querySelector('.profile__title').textContent = inputName;
   document.querySelector('.profile__subtitle').textContent = jobInput;
+
   popup.classList.remove('popup_opend');
 }
 
 showPopupButton.addEventListener('click', popupToggle);
 hidePopupButton.addEventListener('click', popupToggle);
 formElement.addEventListener('submit', formSubmitHandler);
-
